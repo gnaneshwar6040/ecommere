770f036d102912b3dfd00408ae241bf45595a98b
gnanigoud <gnaneshwargoudkoppula@gmail.com>
2025-08-15 18:16:06 +0530

    Login and Signup features

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,16 +1,38 @@
 import { useState } from "react";
+import { validateEmail } from "../utils/utils";
 
 function Login(){
 
+    // use state variables
     var [email,setEmail]=useState("")
     var [pword,setPword]=useState("")
+
+    // error variables
+    var [emailError, setemailError]=useState("")
+    var [pwordError,setpwordError]=useState("")
 
     function handleEmailchange(event){
         setEmail(event.target.value )
 
     }
-      function handlePwordchange(){
-        
+      function handlePwordchange(event){
+        setPword(event.target.value)
+    }
+
+    function handleLogin(){
+        if(validateEmail(email)){
+            setemailError("")
+        }
+        else{
+            setemailError("Email is not valid")
+        }
+
+        if(pword.length < 7){
+            setpwordError("Minimum 8 charcters")
+        }
+        else{
+            setpwordError("")
+        }
     }
 
     return(
@@ -21,12 +43,14 @@ function Login(){
                     <div className="mt-3">
                         <label>Email</label>
                         <input type="email" onChange={event => handleEmailchange(event)} className="form-control" placeholder="Enter Your Email"/>
+                        <div className="text-danger">{emailError}</div>
                     </div>
                      <div className="mt-3">
                         <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Enter Your Password"/>
+                        <input type="password" onChange={event => handlePwordchange(event)} className="form-control" placeholder="Enter Your Password"/>
+                        <div className="text-danger">{pwordError}</div>
                     </div>
-                    <div className="mt-3"> <button className="btn btn-primary">Login</button></div>
+                    <div className="mt-3"> <button className="btn btn-primary" onClick={event => handleLogin()}>Login</button></div>
 
                 </div>
 
--- src/Auth/Signup.js
+++ src/Auth/Signup.js
@@ -1,10 +1,19 @@
 import { useState } from "react";
+import { validateEmail } from "../utils/utils";
 
 function Signup(){
+
+  //use state variables
  var [name , setName]=useState("");
 var [email , setEmail]=useState("");
 var [mobile , setMobile]=useState("");
 var [pword , setPword]=useState("")
+
+//Error variables
+var [nameError, setnameError] = useState("")
+var [emailError, setemailError]=useState("")
+var [mobileError, setmobileError]=useState("")
+var [pwordError, setpwordError]=useState("")
 
 function handleNamechange(event){
 setName(event.target.value)
@@ -21,6 +30,53 @@ function handleMobilechange(event){
 function handlePwordchange(event){
     setPword(event.target.value)
 }
+
+function handleCreateAccount(){
+  var noOfErrors=0;
+
+
+  if(name.length <3){
+    setnameError("minimum 3 characters")
+    noOfErrors++
+  }
+  else{
+    setnameError("")
+  }
+
+  if(validateEmail(email)){
+    setemailError("")
+  }else{
+    setemailError("Email is invalid")
+        noOfErrors++
+
+  }
+  
+  if(mobile.length == 10){
+    setmobileError("")
+  }
+  else{
+    setmobileError("Mobile Number is invalid")
+        noOfErrors++
+
+  }
+
+  if(pword.length<7){
+    setpwordError("Minimum 8 charactors")
+        noOfErrors++
+
+  }
+  else{
+    setpwordError("")
+  }
+
+if(noOfErrors == 0){
+  console.log("Api calling is successfull")
+}
+
+
+
+
+}
     return(
         <div className="container">
              <div className="row">
@@ -28,22 +84,26 @@ function handlePwordchange(event){
                       <h2>Create-Account</h2>
                       <div className="mt-3">
                           <label>Name  </label>
-                          <input type="text" onChange={event => handleNamechange(event)} className="form-control" placeholder="Enter Your Name"/> 
+                          <input type="text" onChange={event => handleNamechange(event)} className="form-control" placeholder="Enter Your Name"/>
+                          <div className="text-danger">{nameError}</div> 
                       </div>
                        <div className="mt-3">
                           <label>Mobile  </label>
                           <input type="text" onChange={event => handleMobilechange(event)} className="form-control" placeholder="Enter Your Number"/> 
+                          <div className="text-danger">{mobileError}</div>
                       </div>
                        <div className="mt-3">
                           <label>Email  </label>
                           <input type="Email" onChange={event => handleEmailchange(event)} className="form-control" placeholder="Enter Your Email"/> 
+                          <div className="text-danger">{emailError}</div>
                       </div>
                        <div className="mt-3">
                           <label>Password  </label>
-                          <input type="Password" onChange={event => handlePwordchange(event)} className="form-control" placeholder="Enter Your Password"/> 
+                          <input type="Password" onChange={event => handlePwordchange(event)} className="form-control" placeholder="Enter Your Password"/>
+                          <div className="text-danger">{pwordError}</div> 
                       </div>
                        <div className="mt-3">
-                          <button className="btn btn-warning">Create Account</button> 
+                          <button className="btn btn-warning" onClick={event => handleCreateAccount()}>Create Account</button> 
                       </div>
                       {
                         name
